d2ed01b6fbdccb3d27bdc1553bfac1a157c9af25
Bobby <bobbielite@pm.me>
2025-10-18 01:02:19 -0400

    Updated code

--- Pages/AuthenticationPreferences.razor.js
+++
@@ -1,9 +0,0 @@
-const pingIdentityScripts = window.pingIdentityScripts;
-
-export const addAuthenticationPreferencesWidget = async (serializedOptions) => {
-    //await pingIdentityScripts.addDaVinciWidget(serializedOptions);
-}
-
-export const dispose = () => {
-    console.debug('Disposing objects.');
-}
